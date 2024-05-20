19a6529050b5051961f3392b47abba1b70e31eed
Yogashree <Yogashree@LAPTOP-IC66QQKO>
2024-05-20 20:09:28 +0530

    fifth commit

--- Hotel Management/src/main/java/com/corsfilter/CorsFilter.java
+++ Hotel Management/src/main/java/com/corsfilter/CorsFilter.java
@@ -13,7 +13,7 @@ public class CorsFilter implements Filter{
 		
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain fc) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-		response.addHeader("Access-Control-Allow-Origin", "https://yogashreed.github.io");
+		response.addHeader("Access-Control-Allow-Origin", "*");
 		response.addHeader("Access-Control-Allow-Headers", "*");
 		response.addHeader("Access-Control-Allow-Methods","GET, POST");
 		fc.doFilter(req,res);
